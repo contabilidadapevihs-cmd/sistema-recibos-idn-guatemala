e3b1d9d164c1c174b69c55fa24481c4421467934
contabilidadapevihs-cmd <contabilidadapevihs@gmail.com>
2026-03-20 11:38:33 -0600

    feat: agregar navegación administrativa

---
+++ components/admin/AdminNav.tsx
@@ -0,0 +1,12 @@
+export function AdminNav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/iglesias">Iglesias</a></li>
+        <li><a href="/ofrendas">Ofrendas</a></li>
+        <li><a href="/reportes">Reportes</a></li>
+      </ul>
+    </nav>
+  );
+}
